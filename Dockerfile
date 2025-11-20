e5359f859decf5f4f520ab93df59df182d22c0cd
Carlson Santana Cruz <carlsonsantana@gmail.com>
2025-11-20 17:54:05 -0300

    Replace Android SDK for apktool

--- Dockerfile
+++ Dockerfile
@@ -20,10 +20,14 @@ COPY openbor /openbor
 WORKDIR /openbor/engine
 RUN ./version.sh
 
+RUN mkdir /apktool && \
+  curl -L https://bitbucket.org/iBotPeaches/apktool/downloads/apktool_2.12.1.jar --output /apktool/apktool_2.12.1.jar
+
 # Install Android Command-line tools
 WORKDIR /
-RUN curl https://dl.google.com/android/repository/commandlinetools-linux-${SDK_VERSION}.zip --output /cmdline-tools.zip && \
-  unzip cmdline-tools.zip && \
+RUN mkdir /android-sdk && \
+  curl -L https://dl.google.com/android/repository/commandlinetools-linux-${SDK_VERSION}.zip --output /android-sdk/cmdline-tools.zip && \
+  unzip /android-sdk/cmdline-tools.zip && \
   mkdir -p /android-sdk/cmdline-tools && \
   mv cmdline-tools /android-sdk/cmdline-tools/latest && \
   cd /android-sdk/cmdline-tools/latest/bin && \
@@ -39,23 +43,25 @@ RUN curl https://dl.google.com/android/repository/commandlinetools-linux-${SDK_V
     -keyalg RSA \
     -dname "CN=gamename.mycompany.com, OU=O, O=O, L=O, S=O, C=US" && \
   printf "storePassword=123456\nkeyPassword=123456\nkeyAlias=a\nstoreFile=/openbor/engine/android/game_certificate.jks\n" > keystore.properties && \
-  ./gradlew assembleRelease && \
+  touch /openbor/engine/android/app/src/main/assets/bor.pak && \
+  ./gradlew assembleRelease --no-daemon --no-build-cache && \
+  archlinux-java set java-17-openjdk && \
+  java -jar /apktool/apktool_2.12.1.jar d /openbor/engine/android/app/build/outputs/apk/release/OpenBOR.apk -o /openbor-android && \
   rm keystore.properties game_certificate.jks /openbor/engine/android/app/build/outputs/apk/release/OpenBOR.apk && \
   rm /openbor/engine/android/app/src/main/res/drawable-hdpi/icon.png && \
   rm /openbor/engine/android/app/src/main/res/drawable-ldpi/icon.png && \
   rm /openbor/engine/android/app/src/main/res/drawable-mdpi/icon.png && \
-  rm /cmdline-tools.zip && \
-  rm -R /android-sdk
+  rm /openbor/engine/android/app/src/main/assets/bor.pak && \
+  rm -R /android-sdk ~/.gradle ~/.android
 
 # Volumes
 RUN mkdir /output
-VOLUME /android-sdk
 VOLUME /game_certificate.jks
 VOLUME /bor.pak
 VOLUME /icon.png
 VOLUME /output
 
 # Run build
-WORKDIR /openbor/engine/android
+WORKDIR /
 COPY run.sh /
 CMD ["bash", "/run.sh"]
--- run.sh
+++ run.sh
@@ -2,49 +2,14 @@
 
 set -e
 
-# Download Android command line tools
-if [ ! -d "/android-sdk/cmdline-tools/latest" ]; then
-  # Remove incomplete files
-  rm -fR /android-sdk/cmdline-tools.zip /android-sdk/cmdline-tools-temp /android-sdk/cmdline-tools
-
-  cd /android-sdk
-  curl https://dl.google.com/android/repository/commandlinetools-linux-${SDK_VERSION}.zip --output /android-sdk/cmdline-tools.zip
-  unzip /android-sdk/cmdline-tools.zip
-  mv cmdline-tools cmdline-tools-temp
-  mkdir cmdline-tools
-  mv cmdline-tools-temp /android-sdk/cmdline-tools/latest
-
-  rm /android-sdk/cmdline-tools.zip
-  touch /android-sdk/tools_not_downloaded
-fi
-
-if [ -f "/android-sdk/tools_not_downloaded" ]; then
-  cd /android-sdk/cmdline-tools/latest/bin
-  archlinux-java set java-17-openjdk
-  echo "y" | ./sdkmanager --install "build-tools;29.0.3" "platform-tools" "platforms;android-29" "tools" "ndk-bundle"
-
-  rm -f /android-sdk/tools_not_downloaded
-fi
-
-archlinux-java set java-11-openjdk
-
 # Remove previous apk build
 rm -f /output/openbor.apk
 
-# Change APK name
-sed -i "s|org\.openbor\.engine|$GAME_APK_NAME|g" /openbor/engine/android/app/build.gradle
-sed -i "s|\"Openbor\"|\"$GAME_NAME\"|g" /openbor/engine/android/app/build.gradle
-
 # Convert icons
-convert /icon.png -resize 72x72 /openbor/engine/android/app/src/main/res/drawable-hdpi/icon.png
-convert /icon.png -resize 36x36 /openbor/engine/android/app/src/main/res/drawable-ldpi/icon.png
-convert /icon.png -resize 48x48 /openbor/engine/android/app/src/main/res/drawable-mdpi/icon.png
+convert /icon.png -resize 72x72 /openbor-android/res/drawable-hdpi/icon.png
+convert /icon.png -resize 36x36 /openbor-android/res/drawable-ldpi/icon.png
+convert /icon.png -resize 48x48 /openbor-android/res/drawable-mdpi/icon.png
 
-cd /openbor/engine/android
-printf "storePassword=$KEYSTORE_STORE_PASSWORD\nkeyPassword=$KEYSTORE_KEY_PASSWORD\nkeyAlias=$KEYSTORE_NAME\nstoreFile=/game_certificate.jks\n" > keystore.properties
+cp /bor.pak /openbor-android/assets/bor.pak
 
-cp /bor.pak /openbor/engine/android/app/src/main/assets/bor.pak
-
-./gradlew assembleRelease
-
-mv /openbor/engine/android/app/build/outputs/apk/release/OpenBOR.apk /output/OpenBOR.apk
+java -jar /apktool/apktool_2.12.1.jar b /openbor-android -o /output/OpenBOR-unsigned.apk
